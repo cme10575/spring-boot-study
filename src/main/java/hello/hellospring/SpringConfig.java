044c1a319cca4cb0d980a53827b1461a322acca3
sujeongKim <cme10575@gmail.com>
2021-07-28 20:41:32 +0900

    스프링 데이터 JPA로 변경

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -5,30 +5,25 @@ import hello.hellospring.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.persistence.EntityManager;
-import javax.sql.DataSource;
-
 @Configuration
 public class SpringConfig {
 
-    private final DataSource dataSource;
-    private final EntityManager em;
+    private final MemberRepository memberRepository;
 
-    public SpringConfig(DataSource dataSource, EntityManager em) {
-        this.dataSource = dataSource;
-        this.em = em;
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     @Bean
     public MemberService memberService(){
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memberRepository() {
-        return new JpaMemberRepository(em);
+    //@Bean
+    //public MemberRepository memberRepository() {
+        //return new JpaMemberRepository(em);
         //return new JdbcTemplateMemberRepository(dataSource);
         //return new JdbcMemberRepository(dataSource);
         //return new MemoryMemberRepository();
-    }
+    //}
 }
