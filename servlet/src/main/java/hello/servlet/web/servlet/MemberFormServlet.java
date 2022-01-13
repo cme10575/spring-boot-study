3f2fd8b83fabea7925d242f3dac11c4646dd987b
sujeongKim <cme10575@gmail.com>
2022-01-13 19:12:50 +0900

    오타 수정

--- servlet/src/main/java/hello/servlet/web/servlet/MemberFormServlet.java
+++ servlet/src/main/java/hello/servlet/web/servlet/MemberFormServlet.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(name = "memberFormServlet", urlPatterns = "/servlet/members/newform")
+@WebServlet(name = "memberFormServlet", urlPatterns = "/servlet/members/new-form")
 public class MemberFormServlet extends HttpServlet {
 
     private MemberRepository memberRepository = MemberRepository.getInstance();
